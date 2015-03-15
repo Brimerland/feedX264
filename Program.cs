318e26bffeb2fce899ae231e3f22e58593dd3953
Brimerland <github@bjoern.gdanietz.com>
2015-03-15 14:56:04 +0100

    blank project

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace feedX264
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var process = new Process();
+                process.StartInfo.Arguments = "-h";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.FileName = "x264/x264-r2538-121396c.exe";
+
+                if (process.Start())
+                {
+                    string s;
+                    while ((s = process.StandardOutput.ReadLine())!=null)
+                        Console.Out.WriteLine(s);
+                }
+                else
+                {
+                    Console.Out.WriteLine("unable to start process");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("Exception: " + e);
+            }
+
+            Console.Out.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ x264/readme.txt
@@ -0,0 +1,4 @@
+Place x264 binary here.
+
+Get it from:
+http://download.videolan.org/pub/videolan/x264/binaries/win64/
